0e3dd9aec2a8f47ba648e715639e8996968ddaff
lmsand <lmsand99@icloud.com>
2024-03-10 04:31:41 -0500

    Comment Input

---
+++ src/screens/CommentsScreen/Input.tsx
@@ -0,0 +1,74 @@
+import {StyleSheet, Text, View, Image, TextInput} from 'react-native';
+import {useState} from 'react';
+import React from 'react';
+import colors from '../../theme/colors';
+import fonts from '../../theme/fonts';
+
+const Input = () => {
+  const [newComment, setNewComment] = useState('');
+
+  const onPost = () => {
+    // sending data to backend
+    setNewComment('')
+  };
+
+  return (
+    <View style={styles.root}>
+      <Image
+        source={{
+          uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg',
+        }}
+        style={styles.image}
+      />
+      <TextInput
+       value={newComment}
+       onChangeText={setNewComment}
+      //  onChangeText={newText => setNewComment(newText)}
+       placeholder="Write your comment"
+       style={styles.input}
+       multiline
+      />
+
+      <Text onPress={onPost} style={styles.button}>
+        POST
+      </Text>
+    </View>
+  );
+};
+
+export default Input;
+
+const styles = StyleSheet.create({
+  root: {
+    flexDirection: 'row',
+    padding: 5,
+    borderTopWidth: 1,
+    borderColor: colors.border,
+    alignItems: 'flex-end'
+  },
+  image: {
+    width: 40,
+    aspectRatio: 1,
+    borderRadius: 20,
+  },
+  input: {
+    flex: 1,
+
+    borderColor: colors.border,
+    borderWidth: 1,
+    borderRadius: 25,
+
+    paddingVertical: 5,
+    paddingRight: 50,
+    paddingHorizontal: 10,
+    marginLeft: 5,
+  },
+  button: {
+    position: 'absolute',
+    right: 15,
+    bottom: 15,
+    fontSize: fonts.size.s,
+    fontWeight: fonts.weight.full,
+    color: colors.primary,
+  },
+});
