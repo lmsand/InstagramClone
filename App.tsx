fdd76b932afe379bbbcf8d9cd50e6b37423d4261
lmsand <lmsand99@icloud.com>
2024-03-10 22:06:36 -0500

    Edit profile ui

--- App.tsx
+++ App.tsx
@@ -2,11 +2,12 @@ import {FlatList, StyleSheet, View} from 'react-native';
 import HomeScreen from './src/screens/HomeScreen/HomeScreen';
 import CommentsScreen from './src/screens/CommentsScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
+import EditProfileScreen from './src/screens/EditProfileScreen';
 
 const App = () => {
   return (
     <View style={styles.app}>
-      <ProfileScreen />
+      <EditProfileScreen />
     </View>
   );
 };
